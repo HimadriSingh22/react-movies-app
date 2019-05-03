48769afe8892f98aad37fea6f591494c1839418c
HimadriSingh22 <rajput.2220.himadri@gmail.com>
2019-05-03 14:58:24 +0530

    added login button and styled login modal

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -22,7 +22,7 @@ const customStyles = {
 
 const TabContainer= function(props){
     return(
-        <Typography component="div" style={{padding:0}}>
+        <Typography component="div" style={{padding:0 , textAlign:'center'}}>
          {props.children}
         </Typography>
     );
@@ -61,7 +61,7 @@ class Header extends Component
             onRequestClose={this.closeModalHandler}
             style={customStyles}
             >
-            <Tabs value={this.state.value}onChange={this.tabChangeHandler}>
+            <Tabs className="tabs" value={this.state.value}onChange={this.tabChangeHandler}>
             <Tab label="Login"/>
             <Tab label="Register"/>
             </Tabs>
@@ -69,11 +69,12 @@ class Header extends Component
              <FormControl required>
                 <InputLabel htmlForm="userName">Username</InputLabel>
                 <Input id="username" type="text"></Input>
-            </FormControl>
+            </FormControl><br/>
             <FormControl>
             <InputLabel htmlForm="password">Password</InputLabel>
                 <Input id="password" type="password"/>
-            </FormControl>
+            </FormControl><br/><br/>
+            <Button variant="contained" color="primary">Login</Button>
             </TabContainer>
             </Modal>
             </div>
