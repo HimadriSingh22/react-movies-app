df6d383d168b64afaf45b541fe9ee93443c4b679
HimadriSingh22 <rajput.2220.himadri@gmail.com>
2019-05-06 15:50:20 +0530

    Added youtube trailer , artist details on the detail page, also the back to home button

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -3,6 +3,12 @@ import Header from '../../common/header/Header';
 import moviesData from '../../assests/movieData';
 import Typography from '@material-ui/core/Typography';
 import './Details.css';
+import ReactDOM from 'react-dom';
+import Home from '../../screens/home/Home';
+import Youtube from 'react-youtube';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 class Details extends Component{
     constructor(){
         super();
@@ -19,11 +25,28 @@ class Details extends Component{
         this.setState({currentState});
         console.log(this.state);
     }
+
+    backtohomeHandler=()=>
+    {
+        ReactDOM.render(<Home/>,document.getElementById('root'));
+    }
     render(){
         let movie=this.state.movie;
+        const opts={
+            height:'300',
+            width:'700',
+            playerVars:{
+                autoplay:1
+            }
+        }
         return(
         <div className="details">
         <Header/>
+        <div className="back">
+        <Typography onClick={this.backtohomeHandler}>
+        Back to Home
+        </Typography>
+        </div>
         <div className="flex-containerDetails">
         <div className="leftDetails">
         <img src={movie.poster_url} alt={movie.title}/>
@@ -32,15 +55,51 @@ class Details extends Component{
         <div>
             <Typography variant="headline" component="h2">{movie.title}</Typography>
         </div>
-        <div>
+            <div>
             <Typography><span className="bold">Genre:</span>{movie.genres.join(',')}</Typography>
+            </div>
+            <div>
+            <Typography > <span className="bold">Duration:</span>{movie.duration}</Typography>
+            </div>
+            <div>
+            <Typography><span className="bold">Release Date:</span>{ new Date(movie.release_date).toDateString()}</Typography>
+            </div>
+            <div>
+            <Typography><span className="bold">Rating:</span>{movie.critics_rating}</Typography>
+            </div>
+        <div className="marginTop16">
+            <Typography><span className="bold">Plot:</span><a href={movie.wiki_url}>(Wiki_link) </a>{movie.storyline}</Typography>
+        </div>
+        <div className="trailerContainer">
+          <Typography><span className="bold">Trailer:</span></Typography>
+          <Youtube
+            videoId={movie.trailer_url.split("?v=")[1]}
+            opts={opts}
+            onReady={this._onReady}/>
         </div>
         </div>
         <div className="rightDetails">
+        <div className="bold marginTop16 marginBottom16">
+        <Typography><span className="bold">Artists:</span></Typography>
+        </div>
+        <div className="paddingRight">
+        
+         <GridList cellHeight={160} cols={2}>
+         {movie.artists.map(artist=>(
+             <GridListTile className="gridTile" key={artist.id}>
+            <a href={artist.wiki_url}><img src={artist.profile_url}  alt={artist.first_name + " "+artist.last_name}/> 
+             <GridListTileBar title={artist.first_name+" "+artist.last_name}/></a>
+
+             </GridListTile>
+         ))}
+             
+         </GridList>
+         </div>
         </div>
         </div>
         </div>
         );
     }
 }
+     
 export default Details;
